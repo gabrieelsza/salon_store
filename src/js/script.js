4af40a97fdbf789e021773e19ce8e3ebf83eb642
Gabriel Souza <biel.leonardo300@gmail.com>
2026-05-23 12:10:56 -0300

    New interactions

--- src/js/script.js
+++ src/js/script.js
@@ -37,3 +37,31 @@ var swiper = new Swiper(".mySwiper", {
         disableOnInteraction: false,
     },
 });
+
+
+const btnMenu = document.getElementById("btn-menu"); 
+const navLinks = document.querySelector(".bar-nav-desktop")
+
+btnMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("show"); 
+    navLinks.classList.toggle("fa-times"); 
+    navLinks.classList.toggle("fa-bars"); 
+
+}); 
+
+const serviceDesc = document.querySelector(".service-desc");
+const galeria = document.querySelector(".galeria");
+
+const observer = new IntersectionObserver((entries) => {
+    if(entries[0].isIntersecting) {
+        entries[0].target.classList.add("show")
+    }  else {
+        entries[0].target.classList.remove("show")
+    } 
+}, {
+    threshold: 0.5, 
+    rootMargin: "0px 0px 0px 400px"
+}); 
+
+observer.observe(serviceDesc);
+observer.observe(galeria);
